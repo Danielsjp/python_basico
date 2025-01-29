b97dfe2332639e66f395f9665549ec2e61323728
daniel.sanchez <sjp.daniel@gmail.com>
2025-01-29 20:11:08 +0100

    Hola

--- 04_strings.py
+++ 04_strings.py
@@ -1,5 +1,12 @@
 # cadenas de texto
-
 string_1 = 'Hola'
 string_2 = "Hola"
 string_3 = """hola"""
+
+nombre = "Daniel"
+apellido = "Sánchez"
+edad = 42
+
+frase = "Me llamo " + nombre + " " + apellido + " y tengo " + str(edad) + " años"
+
+print(frase)
