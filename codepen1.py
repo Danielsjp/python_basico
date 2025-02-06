7401dc44470280e547da32b94695d7883c32112c
daniel.sanchez <sjp.daniel@gmail.com>
2025-02-06 16:15:57 +0100

    hola

--- codepen1.py
+++ codepen1.py
@@ -7,3 +7,20 @@
 # -- Si el número es multiplo de 3 y de 5, escribiremos 3 - FIZZ-BUZZ
 # -- En los demás casos sólo el número
 # -- Si el usuario escribe más de 100 o menos de 0, diremos "El número es incorrecto"
+
+try:
+    entero = int(input("Introduce un numero entero"))
+    if entero >= 0 and entero <= 100:
+        for i in range(0,entero+1):
+            if i % 3 == 0 and entero % 5 == 0 :
+                print(f"{i} FIZZ-BUZ")
+            elif i % 5 == 0 :
+                print(f"{i} BUZZ")
+            elif i % 3 == 0 :
+                print(f"{i} FIZZ")
+            else:
+                print(i)
+    else:
+        print("el número es incorrecto")
+except:
+    print("error")
