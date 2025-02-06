94c449bea5c0f94e16165249b0f5d460f26853b7
daniel.sanchez <sjp.daniel@gmail.com>
2025-02-06 15:39:44 +0100

    hola

--- Exercicis_Python_04_segundos.py
+++ Exercicis_Python_04_segundos.py
@@ -20,9 +20,25 @@ Lo mismo si supera un día o una semana.
 """
 
 print("Conversor de segundos")
-segundos = int(input("Introduzca una cantidad de segundos"))
+seconds = int(input("Introduzca una cantidad de segundos"))
+seconds = seconds % (24 * 3600)
+hour = seconds // 3600
+seconds %= 3600
+minutes = seconds // 60
+seconds %= 60
+
+
 if segundos < 60:
     print(f"{segundos} son menos de 1 minuto")
+
 elif segundos >= 60:
     minutos = segundos / 60
-    print(f"{segundos} son {minutos}")
+    minutos2 = round(minutos, 0)
+    segundos2 = round(minutos - minutos2,2)
+    print(f"{segundos} son {minutos2} minutos y {segundos2*10} segundos")
+
+elif segundos > 3600:
+    horas = segundos / 3600
+    horas2 = round(horas, 0)
+    minutos2 = round(horas - horas2,2)
+    print(f"{segundos} son {horas2} horas y {minutos2*10} minutos")
