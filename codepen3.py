343bbded5fbad987a0d978f1c04020ad9f06d6b1
daniel.sanchez <sjp.daniel@gmail.com>
2025-02-06 19:10:40 +0100

    hola

--- codepen3.py
+++ codepen3.py
@@ -1,6 +1,6 @@
 #tabla de multiplicar
 try:
-    numero_A = int(input("Introduce un numero del 1 al 10 ->"))
+    numero_A = int(input("Introduce un número del 1 al 10 ->"))
     for numero_B in range(1,10+1):
         multi=numero_A*numero_B
         print(f"{numero_A}*{numero_B}={multi}")
---
+++ codepen4.py
@@ -0,0 +1,16 @@
+# Tenemos esta lista de animales:
+
+animales = ["gato", "perro", "caballo", "paloma", "murcielago", "leon", "mono"]
+indicador = False
+# pedir una letra e indicar que animales tienen esa letra
+try:
+    letra = input("Introduce una letra -> ")
+    for i in animales:
+        for a in i:
+            if a == letra:
+                print(i)
+                indicador = True
+    if not indicador:
+        print("Ningún animal contiene esa letra")   
+except:
+    print("error")
