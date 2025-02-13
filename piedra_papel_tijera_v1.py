99526f50094daec9dd18a7e22510ffca0ad2d489
daniel.sanchez <sjp.daniel@gmail.com>
2025-02-13 20:07:25 +0100

    hello

--- piedra_papel_tijera_v1.py
+++ piedra_papel_tijera_v1.py
@@ -1,17 +1,30 @@
+""""
+LISTA MEJORAS
+
+Limitar un maximo de partidas
+Contar cuantas veces han ganado, perdido y empatado
+Preguntar el nombre del usuario
+
+Guardar los resultados del jugador con un ranking
+
+"""
+
 # Informar al usuario de las opciones del juego
 import random
+# Lista de las opciones
+opciones_juego = ["Piedra", "Papel", "Tijeras"]
+opciones_juego = ["💎", "🧻", "✂"]
 
-menu = """
+menu = f"""
 
 PIEDRA - PAPEL - TIJERA
 =======================
 
-1. Piedra
-2. Papel
-3. Tijera
+1. {opciones_juego[0]}
+2. {opciones_juego[1]}
+3. {opciones_juego[2]}
 
 Any Key. Salir
-
 
 
 """
@@ -28,6 +41,11 @@ else:
     print("Quieres jugar")    
 
     opcion_maquina = str(random.randint(1,3))
+    
+    resultado_partida = f"""
+        Has elegido {opciones_juego[int(opcion_humano)-1]}
+        La máquina ha elegido {opciones_juego[int(opcion_maquina)-1]}
+    """
 
     if opcion_humano == opcion_maquina:
         print("Empate")
@@ -35,5 +53,7 @@ else:
         or (opcion_humano == "2" and opcion_maquina == "1") \
             and (opcion_humano == "3" and opcion_maquina == "1"):
         print("Has ganado!")
+        print(resultado_partida)
     else:
         print("Has perdido!")
+        print(resultado_partida)
