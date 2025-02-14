4e13820b4201a5fddb76e6fd4626a0f10021572e
daniel.sanchez <sjp.daniel@gmail.com>
2025-02-14 18:18:57 +0100

    hl

--- entradas_cine.py
+++ entradas_cine.py
@@ -5,6 +5,7 @@ Hay tres precios:
 - Entrada estándar: 9.00
 - Mayores de 65 años (seniors) : 6.90
 - Infantiles : 7.20
+- Dia del espectador: 5
 Se puede vender cualquier cantidad de entradas,
 pero los menores siempre deber ir acompañados
 de un adulto.
@@ -20,14 +21,20 @@ os.system('cls' if os.name == 'nt' else 'clear')
 ticket_estandard = []
 ticket_mayores = []
 ticket_menores = []
+ticket_espectador = []
 
 # precio entradas
 estandard = 9.00
 mayores = 6.90
 infantiles = 7.20
+precio_espectador = 5
+
+precio_espectador_final = 0
 
 # checks
 seguridad = False
+espectador = False
+cantidad = 0
 
 def comprar_entradas(cantidad, case):
     if case == 1:
@@ -36,6 +43,8 @@ def comprar_entradas(cantidad, case):
         ticket_mayores.append(cantidad*mayores)
     if case == 3:
         ticket_menores.append(cantidad*infantiles)
+    if case == 4:
+        ticket_espectador.append(cantidad*precio_espectador)
 
 iterar = True
 
@@ -44,11 +53,12 @@ while iterar:
     print("")
     print("-------TPV D3 ENTRADAS------")
     print("")
-    print("1 .- Entrada estándar: 9.00")
-    print("2 .- Mayores de 65 años (seniors) : 6.90")
-    print("3 .- Infantiles : 7.20")
-    print("4 .- Indicar Importe Total")
-    print("x - para salir")
+    print(f"1 .- Entrada estándar: {estandard} €")
+    print(f"2 .- Mayores de 65 años (seniors) : {mayores} €")
+    print(f"3 .- Mayores menores de 18 años: {infantiles} €")
+    print(f"4 .- Dia del espectador: {precio_espectador} €")
+    print(f"5 .- Indicar Importe Total")
+    print(f"x - para salir")
     print("")
 
     opcion = input("Elige tu opcion --> ")
@@ -85,13 +95,32 @@ while iterar:
                     iterar = False
 
         case '4':
+            case = 4
+            dia = input("¿dia del espectador? Si/No")
+            if dia  == "Si":
+                espectador = True
+                cantidad = round(sum(ticket_estandard)/estandard) + round(sum(ticket_mayores)/mayores) + round(sum(ticket_menores)/infantiles)
+                precio_espectador_final = cantidad * precio_espectador
+            #     pass
+            # else:
+            #     seguridad = input("¿va el menor acompañado responda: Si / No?")
+            #     if seguridad == 'Si':
+            #         cantidad = float(input("¿Cantidad de entradas?"))
+            #         comprar_entradas(cantidad, case)
+            #     else:
+            #         print("no se pueden vender entradas a menores no acompañados")
+            #         #break
+            #         iterar = False
+
+        case '5':
             os.system('cls' if os.name == 'nt' else 'clear')
             print("")
             print("########################################################################")
-            total = sum(ticket_estandard) + sum(ticket_mayores) + sum(ticket_menores)
+            total = sum(ticket_estandard) + sum(ticket_mayores) + sum(ticket_menores) 
             print(f"Cantidad tickets estandard {round(sum(ticket_estandard)/estandard)} total: {sum(ticket_estandard)} Euros")
             print(f"Cantidad tickets mayor edad {round(sum(ticket_mayores)/mayores)} total: {sum(ticket_mayores)} Euros")
             print(f"Cantidad tickets menores {round(sum(ticket_menores)/infantiles)} total: {sum(ticket_menores)} Euros")
+            print(f"Precio total espectador :{precio_espectador_final} Euros")
             print("")
             print(f"-------------------->> Total {total} Euros")
             print("Gracias por su visita")
---
+++ entradas_cine_ferran.py
@@ -0,0 +1,63 @@
+"""
+ENTRADAS DEL CINE
+Vamos a crear una app para vender entradas del cine.
+Hay tres precios:
+- Entrada estándar: 9.00
+- Mayores de 65 años (seniors) : 6.90
+- Infantiles : 7.20
+- Día del espectador: 5.00
+
+
+Se puede vender cualquier cantidad de entradas,
+pero los menores siempre deber ir acompañados
+de un adulto.
+Al finalizar la compra mostraremos las entradas 
+y el importe total. 
+"""
+import os
+
+# Información de partida
+
+tipo_entrada = {"estandard": 9.0, "senior": 6.9, "infantil": 7.2}
+
+# tipo_entrada = {"Estandard": 9.0, "Senior": 6.9, "Infantil": 7.2, "dia del espectador": 5.0}
+
+# Lo que el cliente va a comprar
+
+lista_entradas_compradas = []
+
+compra_activa = True
+
+lista_iniciales = []
+
+for clave in tipo_entrada:
+    lista_iniciales.append(clave[0])
+    
+
+while compra_activa:
+
+    menu = "Precios de la entradas :"
+    for clave, valor in tipo_entrada.items():
+        menu += f"\n{clave[0].upper()}. {clave.capitalize()} : {valor:.2f}€"
+
+    menu += "\n\nF. Finalizar la compra"
+    menu += "\nX. Salir sin compra\n"
+    menu += "\n Elija el tipo de entrada que desea."
+    menu += "\nA continuacion podrà indicar la cantidad. \n"
+
+    eleccion_entrada = input(menu).lower().strip()
+
+    if eleccion_entrada == "x":
+        print("\nAplicación finalizada !Hasta Pronto!")
+        compra_activa = False
+    elif eleccion_entrada == "f":
+        if lista_entradas_compradas:
+            pass
+            # compra_activa = False
+        else:
+            print("Aun no ha realizado la compra")
+    elif eleccion_entrada in lista_iniciales:
+        print("Puedes comprar")
+    else:
+        os.system("cls")
+        print("Opción incorrecta")
