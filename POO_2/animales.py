599a99fa786306455cfc9a05b3fab2f914c0b2c3
daniel.sanchez <sjp.daniel@gmail.com>
2025-02-18 20:08:11 +0100

    commit

--- POO_2/animales.py
+++ POO_2/animales.py
@@ -1,6 +1,9 @@
 class Animal():
-    pass
-
+    def __init__(self, especie):
+        self.especie = especie
+    def __str__(self):
+        #return self.especie
+        return f"la especie es {self.especie}"
 
 # Esta clase va a tener otras sub clases
 
@@ -12,16 +15,21 @@ class Gato(Animal):
     def sonido(self):
         print("Miau")
 
-Buba = Perro()
+Buba = Perro("Dalmata")
 
 Buba.sonido()
 
-Sardina = Gato()
+# Sardina = Gato()
 
-Sardina.sonido()
+# Sardina.sonido()
 
 # para ver de que clase parte otra clase
 print(Perro.__bases__)
 
 #para ver que subclases tiene una clase
 print(Animal.__subclasses__())
+
+
+tortuga = Animal("Tortuga")
+
+print(tortuga)
