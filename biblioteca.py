c50b45855261a3abdd0fbd943ec290b6562d4fd7
daniel.sanchez <sjp.daniel@gmail.com>
2025-02-25 19:12:53 +0100

    hello

---
+++ biblioteca.py
@@ -0,0 +1,99 @@
+"""
+Programación Orientada a Objetos: Biblioteca
+
+El programa debe crear las siguientes clases con sus métodos:
+
+    Clase Lector, que se construirá con nombre y apellido
+
+    Clase Libro, que se construirá con nombre_autor, apellido_autor,
+    y título
+
+    Clase Biblioteca, que se construirá con nombre y dirección
+    Esta clase dispondrá de los siguientes métodos:
+    - agregar_lector: agrega un lector a la biblioteca
+    - mostrar lectores
+    - agregar_libro: agrega un libro a la biblioteca,
+        indicando los ejemplare disponibles
+    - buscar_libro: busca un libro en la biblioteca, 
+        indicando si lo tiene o no
+    - mostrar libros de la biblioteca
+
+
+"""
+
+class Lector():
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+groucho = Lector("Groucho", "Marx")
+
+daniel = Lector("Daniel", "Sanchez")
+
+class Libro():
+    def __init__(self, nombre_autor, apellido_autor, titulo):
+        self.nombre_autor = nombre_autor
+        self.apellido_autor = apellido_autor
+        self.titulo = titulo
+
+el_quijote = Libro("Miguel de Cervantes", "Saavedra", "El quijote")    
+firmin = Libro("Pepito", "Rodriguez", "Firmin")
+
+class Biblioteca():
+    
+    def __init__(self, nombre, dirección):
+        self.nombre = nombre
+        self.dirección = dirección
+        self.lista_lectores = []
+        self.lista_libros = []
+    
+    def agregar_lector(self, cliente: Lector):
+        if cliente in self.lista_lectores:
+              print(f"Ya existe {cliente.nombre}")
+        else:
+            self.lista_lectores.append(cliente)        
+
+    def mostrar_lectores(self):
+       for valor in self.lista_lectores:
+           print(f"{valor.nombre}, {valor.apellido}")
+
+    def agregar_libro(self, libro: Libro):
+        if libro in self.lista_libros:
+            print (f"Ya existe {libro.titulo}")
+        else:
+            self.lista_libros.append(libro)
+            print (f"Se ha agregado el libro {libro.titulo}")
+        
+    def buscar_libro(self, name):
+        self.name = name
+
+        for item in self.lista_libros:
+            if item.titulo == self.name:
+               print(f"Existe {item.titulo}")
+               break
+            else:
+               print(f"No existe {item.titulo}")
+               break   
+
+    def mostrar_libros(self):
+        i=0
+        for item in self.lista_libros:
+            i +=1
+            print(f"{i}-{item.titulo}")
+
+    def __str__(self):
+        pass
+
+sofia_barat = Biblioteca("Sofia Barat", "Carrer Girona 94")
+
+sofia_barat.agregar_lector(groucho)
+sofia_barat.agregar_lector(groucho)
+sofia_barat.agregar_lector(daniel)
+sofia_barat.agregar_lector(daniel)
+sofia_barat.agregar_libro(el_quijote)
+sofia_barat.agregar_libro(firmin)
+sofia_barat.mostrar_lectores()
+sofia_barat.buscar_libro("El quijote")
+sofia_barat.agregar_libro(el_quijote)
+sofia_barat.agregar_libro(firmin)
+sofia_barat.mostrar_libros()
--- restaurante.py
+++ restaurante.py
@@ -16,7 +16,6 @@ Cada uno tiene:
 
 -- clientes:
 
-
 Del cliente vamos a necesitar (de momento)
 
 sólo el nombre
@@ -40,23 +39,28 @@ class Restaurantes():
         self.nombre = nombre
         self.especialidad = especialidad
         self.turnos = turnos
+        
         #self.prog = {13:0,14:0,15:0,16:0,21:0}
         self.reservas = {}
+
         for prog in self.turnos:
             self.reservas[prog] = 0
+
     def Reserva(self, cliente: Cliente, turno):
         self.cliente = cliente
         self.turno = turno
+        
+        # Comprobamos si la hora solicitada esta en los turnos del restaurante.
         if turno in self.turnos:
             if self.reservas[turno]<3:
                 self.reservas[turno] +=1 
                 print(f"El cliente {cliente.name} ha realizado una reserva en el turno {self.turno}")
             else:
-                print("No se pueden realizar mas reservas")    
+                lista_turnos = [ str(turno) for turno in self.turnos ]
+                print(f"No se pueden realizar mas reservas en el turno de {self.turno} de los turnos :" + ", ".join(lista_turnos))    
     
     def __str__(self):
         pass
-
 
 restaurante_1 = Restaurantes("Can Pizza", "Italiana", (13,14,15,16,21))
 
@@ -66,6 +70,10 @@ restaurante_1.Reserva(Cliente("Anna"), 13)
 restaurante_1.Reserva(Cliente("Anna"), 13)
 restaurante_1.Reserva(Cliente("Anna"), 13)
 restaurante_1.Reserva(Cliente("Maria"), 14)
+
+#imprime en un diccionario los parametros pasados y que tiene el objeto creado
+
+print(restaurante_1.__dict__)
 
 
  
