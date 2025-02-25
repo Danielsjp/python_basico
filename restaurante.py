ad76267cd6d8b1ae772777ecda1d82443aced20c
daniel.sanchez <sjp.daniel@gmail.com>
2025-02-25 16:07:53 +0100

    test

--- restaurante.py
+++ restaurante.py
@@ -1,4 +1,5 @@
 """
+
 Vamos a gestionar restaurantes
 
 Cada uno tiene:
@@ -26,36 +27,38 @@ cliente_1 = Cliente("Anna")
 
 restaurante_1 = Restaurante("Can Pizza", "Italiana", [13,14,15,16,21])
 
+
 """
+
 class Cliente():
-    def __init__(self, nombre: list):
-        self.nombre = nombre
+    def __init__(self, nombre):
+        self.name = nombre
 
 class Restaurantes():
-    def __init__(self, nombre, especialidad, turnos: tuple):
+    def __init__(self, nombre, especialidad, turnos: list):
+        
         self.nombre = nombre
         self.especialidad = especialidad
         self.turnos = turnos
-        self.prog = {13:0,14:0,15:0,16:0,21:0}
+        #self.prog = {13:0,14:0,15:0,16:0,21:0}
+        self.reservas = {}
+        for prog in self.turnos:
+            self.reservas[prog] = 0
     def Reserva(self, cliente: Cliente, turno):
         self.cliente = cliente
         self.turno = turno
         if turno in self.turnos:
-            if self.prog[turno]<3:
-                self.prog[turno] = self.prog[turno] + 1
-                print(f"El cliente {cliente.nombre} ha realizado una reserva en el turno {self.turno}")
+            if self.reservas[turno]<3:
+                self.reservas[turno] +=1 
+                print(f"El cliente {cliente.name} ha realizado una reserva en el turno {self.turno}")
             else:
                 print("No se pueden realizar mas reservas")    
     
     def __str__(self):
         pass
 
-class Cliente():
-    def __init__(self, nombre):
-        self.nombre = nombre
 
-
-restaurante_1 = Restaurantes("Can Pizza", "Italiana", [13,14,15,16,21])
+restaurante_1 = Restaurantes("Can Pizza", "Italiana", (13,14,15,16,21))
 
 restaurante_1.Reserva(Cliente("Anna"), 13)
 restaurante_1.Reserva(Cliente("Maria"), 14)
